63ab2410743e15073bc14d5000609ba3035e4f78
Kathy <cali.mcclung@broadcom.com>
2022-05-26 14:19:16 -0700

    US155 is the bomb!

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 /* is this a comment? */
 /* This is for Rally */
-/* This is for Rally  2 + 3 + 4 */
+/* This is for Rally  2 + 3 + 4. + 5 */
 var util = require("util");
 console.log ("Hello world");
 util.print("Hello, world!");
