84ca9b8b5febfa3aa3c91523d4fda321757fc5e3
Kathy <cali.mcclung@broadcom.com>
2022-05-26 14:07:57 -0700

    US155 third time the charm

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 /* is this a comment? */
 /* This is for Rally */
-/* This is for Rally  2 */
+/* This is for Rally  2 + 3 */
 var util = require("util");
 console.log ("Hello world");
 util.print("Hello, world!");
